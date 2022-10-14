bc48a540cbf71005fda2a31e8bf0d16b86b3ab4a
oklymenko <olexandr.klymenko@gmail.com>
2022-10-14 12:00:12 +0300

    fix

--- cpu-benchmark.py
+++ cpu-benchmark.py
@@ -8,16 +8,16 @@ import cpuinfo
 os_version = platform.system()
 
 print('Python CPU Benchmark by Alex Dedyura (Windows, macOS(Darwin), Linux)')
-print('CPU: ' + cpuinfo.get_cpu_info()['brand_raw'])
-print('Arch: ' + cpuinfo.get_cpu_info()['arch_string_raw'])
+print('CPU: ' + cpuinfo.get_cpu_info().get('brand_raw', "Unknown"))
+print('Arch: ' + cpuinfo.get_cpu_info().get('arch_string_raw', "Unknown"))
 print('OS: ' + str(os_version))
 
 print('\nBenchmarking: \n')
 
-start_benchmark = 10000 # change this if you like (sample: 1000, 5000, etc)
+start_benchmark = 100 # change this if you like (sample: 1000, 5000, etc)
 start_benchmark = int(start_benchmark)
 
-repeat_benchmark = 10 # attemps, change this if you like (sample: 3, 5, etc)
+repeat_benchmark = 5 # attemps, change this if you like (sample: 3, 5, etc)
 repeat_benchmark = int(repeat_benchmark)
 
 average_benchmark = 0
